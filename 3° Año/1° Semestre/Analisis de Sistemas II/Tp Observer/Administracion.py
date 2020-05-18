1a8c027a5208e6885425610bd5e538605bdb9c7c
Lucas <l.soria@alumno.um.edu.ar>
2020-05-18 00:16:37 -0300

    TP Python analisis

---
+++ 3° Año/1° Semestre/Analisis de Sistemas II/Tp Observer/Administracion.py
@@ -0,0 +1,7 @@
+from ILibroMalEstado import ILibroMalEstado
+
+
+class Administracion(ILibroMalEstado):
+
+    def update(self):
+        print("\nAdministracion:\n\tEnvio una queja formal...")
---
+++ 3° Año/1° Semestre/Analisis de Sistemas II/Tp Observer/Subject.py
@@ -0,0 +1,10 @@
+class Subject:
+
+    def attach(self, observador):
+        pass
+
+    def dettach(self, observador):
+        pass
+    
+    def notifyObservers(self):
+        pass
