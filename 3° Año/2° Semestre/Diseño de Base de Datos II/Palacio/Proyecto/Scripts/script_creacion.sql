a388798ee2720f4399be98c112ac9fd43aaa1fc5
Lucas <l.soria@alumno.um.edu.ar>
2020-09-17 21:12:19 -0300

    Ni idea

--- 3° Año/2° Semestre/Diseño de Base de Datos II/Palacio/Proyecto/Scripts/script_creacion.sql
+++ 3° Año/2° Semestre/Diseño de Base de Datos II/Palacio/Proyecto/Scripts/script_creacion.sql
@@ -1,10 +1,10 @@
 USE master;
 
-DROP DATABASE IF EXISTS PedidosYaArgentina;
+DROP DATABASE IF EXISTS ComiditasYa;
 
-CREATE DATABASE PedidosYaArgentina;
+CREATE DATABASE ComiditasYa;
 
-USE PedidosYaArgentina;
+USE ComiditasYa;
 
 CREATE TABLE Provincia (
     id_provincia int NOT NULL IDENTITY(1,1),
@@ -43,31 +43,32 @@ CREATE TABLE Repartidor (
 	CONSTRAINT FK_Repartidor_Departamento FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento) ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
-CREATE TABLE Tipo_establecimiento (
-    id_tipo_establecimiento int NOT NULL IDENTITY(1,1),
-    descripcion varchar(50),
-    PRIMARY KEY (id_tipo_establecimiento),
+CREATE TABLE Tipo_restaurante (
+    id_tipo_restaurante int NOT NULL IDENTITY(1,1),
+    tipo_empresa varchar(50) NOT NULL,
+    tipo_restaurante varchar(50) NOT NULL,
+    PRIMARY KEY (id_tipo_restaurante),
 );
 
-CREATE TABLE Establecimiento (
-    id_establecimiento int NOT NULL IDENTITY(1,1),
-    descripcion varchar(50),
+CREATE TABLE Restaurante (
+    id_restaurante int NOT NULL IDENTITY(1,1),
+    nombre varchar(50),
     id_departamento int NOT NULL,
-    id_tipo_establecimiento int NOT NULL,
-    PRIMARY KEY (id_establecimiento),
-    CONSTRAINT FK_Establecimiento_Departamento FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FK_Establecimiento_Tipo_establecimiento FOREIGN KEY (id_tipo_establecimiento) REFERENCES Tipo_establecimiento(id_tipo_establecimiento) ON DELETE CASCADE ON UPDATE CASCADE,
+    id_tipo_restaurante int NOT NULL,
+    PRIMARY KEY (id_restaurante),
+    CONSTRAINT FK_Restaurante_Departamento FOREIGN KEY (id_departamento) REFERENCES Departamento(id_departamento) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_Testaurante_Tipo_restaurante FOREIGN KEY (id_tipo_restaurante) REFERENCES Tipo_restaurante(id_tipo_restaurante) ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
 CREATE TABLE Tipo_comida (
     id_tipo_comida int NOT NULL IDENTITY(1,1),
-    descripcion varchar(50),
+    tipo_comida varchar(50) NOT NULL,
     PRIMARY KEY (id_tipo_comida)
 );
 
 CREATE TABLE Comida (
     id_comida int NOT NULL IDENTITY(1,1),
-    descripcion varchar(50),
+    nombre varchar(50) NOT NULL,
     precio int NOT NULL,
     id_tipo_comida int NOT NULL,
     PRIMARY KEY (id_comida),
@@ -75,20 +76,21 @@ CREATE TABLE Comida (
 );
 
 
-CREATE TABLE Establecimiento_comida (
+CREATE TABLE Restaurante_comida (
     id_comida int NOT NULL,
-    id_establecimiento int NOT NULL,
-    PRIMARY KEY (id_comida, id_establecimiento),
+    id_restaurante int NOT NULL,
+    PRIMARY KEY (id_comida, id_restaurante),
     CONSTRAINT FK_EC_Comida FOREIGN KEY (id_comida) REFERENCES Comida(id_comida) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT FK_EC_Establecimiento FOREIGN KEY (id_establecimiento) REFERENCES Establecimiento(id_establecimiento) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT FK_EC_Restaurante FOREIGN KEY (id_restaurante) REFERENCES Restaurante(id_restaurante) ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
 CREATE TABLE Pedido (
     id_pedido int NOT NULL IDENTITY(1,1),
-    fecha smalldatetime,
+    fecha smalldatetime NOT NULL,
     id_cliente int NOT NULL,
     id_comida int NOT NULL,
     id_repartidor int NOT NULL,
+	importe_envio int NOT NULL,
     PRIMARY KEY (id_pedido),
     CONSTRAINT FK_Pedido_Cliente FOREIGN KEY (id_cliente) REFERENCES Cliente(id_cliente),
     CONSTRAINT FK_Pedido_Comida FOREIGN KEY (id_comida) REFERENCES Comida(id_comida),
