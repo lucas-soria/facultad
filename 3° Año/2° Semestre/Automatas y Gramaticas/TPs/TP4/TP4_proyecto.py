24fc32eab073ca20f9352c2810dc4c48c0d7d6e7
Lucas <l.soria@alumno.um.edu.ar>
2020-11-16 13:24:57 -0300

    TP4 automatas y gramaticas

--- 3° Año/2° Semestre/Automatas y Gramaticas/TPs/TP4/TP4_proyecto.py
+++ 3° Año/2° Semestre/Automatas y Gramaticas/TPs/TP4/TP4_proyecto.py
@@ -7,51 +7,124 @@ import re
 from pprint import pprint
 
 
-# No hay dias feriado no laborables del 28/08 al 04/10
-# preguntar por alguna fecha que interese
-patron = re.compile(r'^(?P<idcon>[0-9a-z]{16});(?P<usuario>(.+));(?P<fechaini>(([0-2][0-9])|(3[01]))\/(08|09|10)\/2019 ((1?[0-9])|(2[0-3])):([0-5][0-9]));(?P<fechafin>(([0-2][0-9])|(3[01]))\/(08|09|10)\/2019 ((1?[0-9])|(2[0-3])):([0-5][0-9]));(?P<tiempo>(\d+));(?P<input>(\d+));(?P<output>(\d+));(?P<macap>(([A-F0-9]{2}-){5}[A-F0-9]{2}:UM));(?P<maccliente>(([A-F0-9]{2}-){5}[A-F0-9]{2}))$')
-with open('./acts-user1.txt', 'r') as registros:
-    registros.readline()
-    linea = registros.readline()
-    c = 1
-    fechas = {}
-    usuarios = []
-    macclientes = []
-    macaps = []
-    while linea:
-        match = patron.match(linea)
-        if match:
-            fecha = match.group('fechaini').split(' ')[0]
-            usuario = match.group('usuario').lower().strip(' ')
-            dia = datetime.strptime(fecha, '%d/%m/%Y').strftime('%A')
-            if usuario not in usuarios:
-                usuarios.append(usuario)
-            if match.group('macap') not in macaps:
-                macaps.append(match.group('macap'))
-            if match.group('maccliente') not in macclientes:
-                macclientes.append(match.group('maccliente'))
-            if dia == 'Saturday' or dia == 'Sunday':
-                if fecha in fechas.keys():
-                    if usuario not in fechas[fecha]:
-                        fechas[fecha].append(usuario)
-                else:
-                    fechas[fecha] = [usuario]
-        else:
-            c += 1
-        linea = registros.readline()
+# No hay dias feriado no laborables del 28/08 al 04/10 asi que decidimos agregar las fechas del año nuevo judio
+feriados = {'29/09/2019': 'Año nuevo Judio, día 1',
+            '30/09/2019': 'Año nuevo Judio, día 2',
+            '01/10/2019': 'Año nuevo Judio, día 3'}
+
+
+patron = re.compile(r'^(?P<idcon>[0-9a-z]{16});(?P<usuario>(.+));(?P<fechaini>(([0-2][0-9])|(3[01]))\/(0[1-9]|1[0-2])\/\d{4} ((1?[0-9])|(2[0-3])):([0-5][0-9]));(?P<fechafin>(([0-2][0-9])|(3[01]))\/(0[1-9]|1[0-2])\/\d{4} ((1?[0-9])|(2[0-3])):([0-5][0-9]));(?P<tiempo>(\d+));(?P<input>(\d+));(?P<output>(\d+));(?P<macap>(([A-F0-9]{2}-){5}[A-F0-9]{2}:UM));(?P<maccliente>(([A-F0-9]{2}-){5}[A-F0-9]{2}))$')
+
+
+def calendarios(fe):
+    print('\nMeses a analizar:\n')
+    tuplas = []
     calendario = calendar.TextCalendar(calendar.MONDAY)
-    print('\nIntegrantes del grupo:\n\t* Marotta, Alenjandro Adrian\n\t* Soria Gava, Lucas Damian\n\n+------------------------------------+\n\nMeses a analizar:\n')
-    calendario.prmonth(2019, 8)
-    print('\n')
-    calendario.prmonth(2019, 9)
-    print('\n')
-    calendario.prmonth(2019, 10)
-    print('\n+------------------------------------+\n\nUsuarios que se conectaron en fines de semana y feriados:\n')
-    pprint(fechas)
-    print(f'\n+------------------------------------+\n\nRegistros incorrectos: {c}\n')
-    print('+------------------------------------+\n\nUsuarios:')
-    pprint(usuarios)
-    print('\nMACs AP:')
-    pprint(macaps)
-    print('\nMACs Cliente:')
-    pprint(macclientes)
+    for i in fe:
+        tupla = (i.split('/')[1], i.split('/')[2])
+        if tupla not in tuplas:
+            tuplas.append(tupla)
+    for i in tuplas:
+        calendario.prmonth(int(i[1]), int(i[0]))
+        print('')
+
+
+def conexiones(fe, fechas):
+    print('+------------------------------------+\n')
+    while True:
+        respuesta = input('¿Desea visualizar la lista completa de conexiones en dias feriados y fines de semana? (Y/N) ')
+        if respuesta.lower() == 'y':
+            calendarios(fe)
+            print('\nUsuarios que se conectaron en fines de semana y feriados:\n')
+            for i in fe:
+                print(f'{i}: {fechas[i][0]}')
+                for j in fechas[i][1]:
+                    print(f'\t{j}')
+                print('')
+            break
+        elif respuesta.lower() == 'n':
+            break
+        else:
+            print('Opción invalida')
+
+
+def buscador(fe, fechas):
+    while True:
+        respuesta = input('\n¿Le interesa conocer los dias feriados y fines de semana en que un usuario especifico se conectó? (Y/N) ')
+        if respuesta.lower() == 'y':
+            user = input('Coloque el nombre de usuario: ')
+            print('\nEl usuario se conectó los días:')
+            for i in fe:
+                if user in fechas[i][1]:
+                    print(f'\t- {i}')
+        elif respuesta.lower() == 'n':
+            break
+        else:
+            print('Opción invalida')
+
+
+def macs(usuarios, macaps, macclientes):
+    print('+------------------------------------+\n')
+    while True:
+        respuesta = input('¿Le interesa ver todas las MAC y usuarios? (Y/N) ')
+        if respuesta.lower() == 'y':
+            print('\nUsuarios:')
+            pprint(usuarios)
+            print('\nMACs AP:')
+            pprint(macaps)
+            print('\nMACs Cliente:')
+            pprint(macclientes)
+            break
+        elif respuesta.lower() == 'n':
+            break
+        else:
+            print('Opción invalida')
+
+
+def main():
+    with open('Wifi.txt', 'r') as registros:
+        registros.readline()
+        linea = registros.readline()
+        c = 1
+        fechas = {}
+        usuarios = []
+        macclientes = []
+        macaps = []
+        while linea:
+            match = patron.match(linea)
+            if match:
+                fecha = match.group('fechaini').split(' ')[0]
+                usuario = match.group('usuario').lower().strip(' ')
+                fecha = datetime.strptime(fecha, '%d/%m/%Y')
+                dia = fecha.strftime('%A')
+                fecha = fecha.strftime('%d/%m/%Y')
+                if usuario not in usuarios:
+                    usuarios.append(usuario)
+                if match.group('macap') not in macaps:
+                    macaps.append(match.group('macap'))
+                if match.group('maccliente') not in macclientes:
+                    macclientes.append(match.group('maccliente'))
+                if dia == 'Saturday' or dia == 'Sunday' or fecha in feriados.keys():
+                    if fecha in fechas.keys():
+                        if usuario not in fechas[fecha][1]:
+                            fechas[fecha][1].append(usuario)
+                    else:
+                        if (dia == 'Saturday' or dia == 'Sunday') and fecha in feriados.keys():
+                            fechas[fecha] = [dia + ' - ' + feriados[fecha], [usuario]]
+                        elif dia == 'Saturday' or dia == 'Sunday':
+                            fechas[fecha] = [dia, [usuario]]
+                        else:
+                            fechas[fecha] = [feriados[fecha], [usuario]]
+            else:
+                c += 1
+            linea = registros.readline()
+        print('\nIntegrantes del grupo:\n\t* Marotta, Alenjandro Adrian\n\t* Soria Gava, Lucas Damian\n')
+        print(f'\n+------------------------------------+\n\nRegistros incorrectos: {c}\n')
+        macs(usuarios, macaps, macclientes)
+        fe = fechas.keys()
+        conexiones(fe, fechas)
+        buscador(fe, fechas)
+
+
+if __name__ == "__main__":
+    main()
--- 3° Año/2° Semestre/Automatas y Gramaticas/TPs/TP4/pruebas.py
+++ 3° Año/2° Semestre/Automatas y Gramaticas/TPs/TP4/pruebas.py
@@ -1,21 +1,13 @@
 import re
 
 
-patron = re.compile(r'''^(?P<idcon>[0-9a-z]{16});\
-(?P<usuario>[a-zA-Z0-9\-\_\.]+);\
-(?P<fechaini>(([0-2][0-9])|(3[01]))\/(08|09|10)\/2019\b(([01][0-9])|(2[0-3])):([0-5][0-9]));\
-(?P<fechafin>(([0-2][0-9])|(3[01]))\/(08|09|10)\/2019\b(([01][0-9])|(2[0-3])):([0-5][0-9]));\
-(?P<tiempo>(\d+));\
-(?P<input>(\d+));\
-(?P<output>(\d+));\
-(?P<macap>(([A-F0-9]{2}-){5}[A-F0-9]{2}:UM));\
-(?P<maccliente>(([A-F0-9]{2}-){5}[A-F0-9]{2}))$''')
+patron = re.compile(r'^(?P<tiempo>(\d+))$')
 
-linea = "b83a1489ae7fbdc5;invitede-pansew;28/08/2019 10:06;26/09/2019 12:48;119;184328;3323107;04-18-D6-21-38-E1:UM;60-1D-91-78-43-74"
+linea = "-254211"
 p = patron.match(linea)
 if p:
     print("{} es correcta\n".format(linea))
-    print(p.group('idcon'), p.group('usuario'), p.group('fechaini'), p.group('fechafin'), p.group('tiempo'), p.group('input'), p.group('output'), p.group('macap'), p.group('maccliente'))
+    print(p.group('tiempo'))
 else:
     print("{} NO es correcta\n".format(linea))
 
