15566df9c768bb2ad76e6a10db3132da1666bb4e
Lucas Soria <l.soria@alumno.um.edu.ar>
2021-04-28 04:26:36 -0300

    Caso2 automatizado terminado

--- 4° Año/1° Semestre/Teleinformatica/Caso 2/caso2-automatizado.py
+++ 4° Año/1° Semestre/Teleinformatica/Caso 2/caso2-automatizado.py
@@ -1,80 +1,118 @@
 #!/usr/bin/python
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSController
-from mininet.node import CPULimitedHost, Host, Node
-from mininet.node import OVSKernelSwitch, UserSwitch
-from mininet.node import IVSSwitch
+from mininet.node import Host, Node
+from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-from mininet.link import TCLink, Intf
-from subprocess import call
 
 
-class Caso2:
+class Star:
 
-    def __init__(self, numberOfNetworks, firstNetwork='192.168.100.0', mask=29, insideNetwork='10.0'):
-        self.net = Mininet(topo=None, build=False, ipBase='10.0.0.0/8')
+    def __init__(self, numberOfNetworks):
         self.numberOfNetworks = numberOfNetworks
-        self.firstNetwork = firstNetwork
-        self.mask = mask
-        self.IPs = None
-        self.insideNetwork = insideNetwork
-        self.rc = self.net.addHost('rc', cls=Node, defaultRoute=None)
-        self.rc.cmd('sysctl -w net.ipv4.ip_forward=1')
-
+        self.net = Mininet(topo=None, build=False, ipBase='10.0.0.0/8')
+        self.rc = None
 
     def networkIPs(self):
-        bitsIPs = 32 - self.mask
-        self.IPs = 2 ** bitsIPs
-        nets = self.firstNetwork.rsplit('.', 1)
+        IPs = 8
+        nets = '192.168.100'
         networks = []
         for network in range(self.numberOfNetworks):
-            networks.append(nets[0] + '.' + str(int(nets[1]) + self.IPs * network))
+            networks.append(nets + '.' + str(int(IPs * network)))
         return networks
 
+    def genSwitch(self, where, number):
+        name = 's' + where + str(number)
+        self.net.addSwitch(name, cls=OVSKernelSwitch, failMode='standalone')
 
-    def createRouterSuc(self, number, networkIP):
+    def genRouter(self, number, networkIP):
         name = 'rsuc' + str(number)
-        prefix = networkIP.rsplit('.', 1)
-        IP = prefix[0] + '.' + str(int(prefix[1]) + 1)
-        rcIP = prefix[0] + '.' + str(int(prefix[1]) + self.IPs - 2)
-        rsucIP = self.insideNetwork + '.' + str(number) + '.1'
-        hname = 'hsuc' + str(number)
-        hsucIP = self.insideNetwork + '.' + str(number) + '.254'
-        allt = [name, IP, rcIP, rsucIP, hname, hsucIP]
-        r = self.net.addHost(name, cls=Node, defaultRoute=None)
+        r = self.net.addHost(name, cls=Node)
         r.cmd('sysctl -w net.ipv4.ip_forward=1')
-        return(allt)
 
+    def genHost(self, number):
+        name = 'h1suc' + str(number)
+        prefix = '10.0.' + str(number)
+        IP = prefix + '.254/24'
+        IPr = prefix + '.1'
+        default = 'via ' + IPr
+        self.net.addHost(name, cls=Host, ip=IP, defaultRoute=default)
 
-    def myNetwork(self):
+    def genLinks(self, number, networkIP):
+        sc = self.net.get('sc' + str(number))
+        rs = self.net.get('rsuc' + str(number))
+        ss = self.net.get('ssuc' + str(number))
+        h = self.net.get('h1suc' + str(number))
+        self.net.addLink(self.rc, sc)
+        self.net.addLink(sc, rs)
+        self.net.addLink(rs, ss)
+        self.net.addLink(ss, h)
+
+    def genNetwork(self):
         networks = self.networkIPs()
-        nets = []
-        for router in range(len(networks)):
-            nets.append(self.createRouterSuc(router + 1, networks[router]))
-        for net in nets:
-            
-        # r.setIP(IP, prefixLen=29, intf=name + "-eth0")
-        # r.setIP(rsucIP, prefixLen=24, intf=name + "-eth1")
-        # hsuc = self.net.addHost(hname, cls=Host, ip=hsucIP, defaultRoute='via ' + rsucIP)
-
-
-
-
-        info( '* Starting network\n')
+        info('*Adding switches\n')
+        for index in range(len(networks)):
+            self.genSwitch('c', index+1)
+            self.genSwitch('suc', index+1)
+        info('*Adding routers and hosts\n')
+        self.rc = self.net.addHost('rc', cls=Node, defaultRoute=None)
+        self.rc.cmd('sysctl -w net.ipv4.ip_forward=1')
+        for index in range(len(networks)):
+            self.genRouter(index+1, networks[index])
+            self.genHost(index+1)
+        info('*Adding links\n')
+        for index in range(len(networks)):
+            self.genLinks(index+1, networks[index])
+        info('* Starting network\n')
         self.net.build()
-        info( '* Starting controllers\n')
-        for controller in self.net.controllers:
-            controller.start()
-
-        info( '* Post configure switches and hosts\n')
-
+        info('* Starting switches\n')
+        for switch in self.net.switches:
+            switch.start([])
+        info('* Post configure switches and hosts\n')
+        for index in range(len(networks)):
+            rs = self.net.get('rsuc' + str(index+1))
+            #  rc's IP for rsuc's network
+            network = networks[index].rsplit('.', 1)
+            IP = network[0] + '.' + str(int(network[1]) + 6)
+            intfrc = 'rc-eth' + str(index)
+            self.rc.setIP(IP, prefixLen=29, intf=intfrc)
+            # rsuc's IPs for its networks
+            IPout = network[0] + '.' + str(int(network[1]) + 1)
+            intf0 = 'rsuc' + str(index + 1) + '-eth0'
+            IPin = '10.0.' + str(index + 1) + '.1'
+            IPinNet = '10.0.' + str(index + 1) + '.0/24'
+            intf1 = 'rsuc' + str(index + 1) + '-eth1'
+            rs.setIP(IPout, prefixLen=29, intf=intf0)
+            rs.setIP(IPin, prefixLen=24, intf=intf1)
+            # rc's route to rsuc
+            cmd = 'ip route add ' + IPinNet + ' via ' + IPout + ' dev rc-eth' + str(index)
+            self.rc.cmd(cmd)
+            # rsuc's route to the other networks
+            # cmd = 'ip route add 0.0.0.0/0 via ' + IP + ' dev ' + intf0
+            cmd = 'ip route add 10.0.0.0/18 via ' + IP + ' dev ' + intf0
+            rs.cmd(cmd)
+            cmd = 'ip route add 192.168.100.0/24 via ' + IP + ' dev ' + intf0
+            rs.cmd(cmd)
         CLI(self.net)
         self.net.stop()
 
 
+def main():
+    while True:
+        while True:
+            try:
+                numberOfNetworks = int(input('How many networks would you like (up to 32)? '))
+                break
+            except Exception:
+                print("Please enter a number")
+        if numberOfNetworks > 0 and numberOfNetworks < 33:
+            break
+        else:
+            print("Please enter a number between 1 and 32")
+    caso2 = Star(numberOfNetworks)
+    caso2.genNetwork()
+
+
 if __name__ == '__main__':
-    setLogLevel( 'info' )
-    numberOfNetworks = int(input('How many networks would you like? '))
-    caso2 = Caso2(numberOfNetworks)
-    caso2.myNetwork()
+    setLogLevel('info')
+    main()
--- 4° Año/1° Semestre/Teleinformatica/Caso 2/caso2.py
+++ 4° Año/1° Semestre/Teleinformatica/Caso 2/caso2.py
@@ -1,44 +1,40 @@
 #!/usr/bin/python
-
 from mininet.net import Mininet
-from mininet.node import Controller, RemoteController, OVSController
-from mininet.node import CPULimitedHost, Host, Node
-from mininet.node import OVSKernelSwitch, UserSwitch
-from mininet.node import IVSSwitch
+from mininet.node import Host, Node
+from mininet.node import OVSKernelSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
-from mininet.link import TCLink, Intf
-from subprocess import call
+
 
 def myNetwork():
 
-    net = Mininet( topo=None,
-                   build=False,
-                   ipBase='10.0.0.0/8')
+    net = Mininet(topo=None,
+                  build=False,
+                  ipBase='10.0.0.0/8')
 
-    info( '* Adding controller\n' )
-    info( '* Add switches\n')
+    info('* Adding controller\n')
+    info('* Add switches\n')
     s3 = net.addSwitch('s3', cls=OVSKernelSwitch, failMode='standalone')
     s2 = net.addSwitch('s2', cls=OVSKernelSwitch, failMode='standalone')
     s4 = net.addSwitch('s4', cls=OVSKernelSwitch, failMode='standalone')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch, failMode='standalone')
 
-    info( '* Add hosts\n')
-    
+    info('* Add hosts\n')
+
     rc = net.addHost('rc', cls=Node, ip='192.168.100.6/29', defaultRoute=None)
     rc.cmd('sysctl -w net.ipv4.ip_forward=1')
-    
+
     rsuc1 = net.addHost('rsuc1', cls=Node, ip='192.168.100.1/29', defaultRoute=None)
     rsuc1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
     rsuc2 = net.addHost('rsuc2', cls=Node, ip='192.168.100.9/29', defaultRoute=None)
     rsuc2.cmd('sysctl -w net.ipv4.ip_forward=1')
-    
+
     hsuc2 = net.addHost('hsuc2', cls=Host, ip='10.0.2.254/24', defaultRoute='via 10.0.2.1')
-    
+
     hsuc1 = net.addHost('hsuc1', cls=Host, ip='10.0.1.254/24', defaultRoute='via 10.0.1.1')
 
-    info( '* Add links\n')
+    info('* Add links\n')
     net.addLink(rc, s1)
     net.addLink(s1, rsuc1)
     net.addLink(rsuc1, s3)
@@ -48,14 +44,13 @@ def myNetwork():
     net.addLink(rsuc2, s4)
     net.addLink(s4, hsuc2)
 
-    rc.setIP("192.168.100.6",prefixLen = 29,intf = "rc-eth0")
-    rc.setIP("192.168.100.14",prefixLen = 29,intf = "rc-eth1")
-    rsuc1.setIP("192.168.100.1",prefixLen = 29,intf = "rsuc1-eth0")
-    rsuc1.setIP("10.0.1.1",prefixLen = 24,intf = "rsuc1-eth1")
-    rsuc2.setIP("192.168.100.9",prefixLen = 29,intf = "rsuc2-eth0")
-    rsuc2.setIP("10.0.2.1",prefixLen = 24,intf = "rsuc2-eth1")
+    rc.setIP("192.168.100.6", prefixLen=29, intf="rc-eth0")
+    rc.setIP("192.168.100.14", prefixLen=29, intf="rc-eth1")
+    rsuc1.setIP("192.168.100.1", prefixLen=29, intf="rsuc1-eth0")
+    rsuc1.setIP("10.0.1.1", prefixLen=24, intf="rsuc1-eth1")
+    rsuc2.setIP("192.168.100.9", prefixLen=29, intf="rsuc2-eth0")
+    rsuc2.setIP("10.0.2.1", prefixLen=24, intf="rsuc2-eth1")
 
-    
     rc.cmd('ip route add 10.0.1.0/24 via 192.168.100.1 dev rc-eth0')
     rc.cmd('ip route add 10.0.2.0/24 via 192.168.100.9 dev rc-eth1')
 
@@ -65,24 +60,24 @@ def myNetwork():
     rsuc2.cmd('ip route add 192.168.100.0/29 via 192.168.100.14 dev rsuc2-eth0')
     rsuc2.cmd('ip route add 10.0.1.0/24 via 192.168.100.14 dev rsuc2-eth0')
 
-    
-    info( '* Starting network\n')
+    info('* Starting network\n')
     net.build()
-    info( '* Starting controllers\n')
+    info('* Starting controllers\n')
     for controller in net.controllers:
         controller.start()
 
-    info( '* Starting switches\n')
+    info('* Starting switches\n')
     net.get('s3').start([])
     net.get('s2').start([])
     net.get('s4').start([])
     net.get('s1').start([])
 
-    info( '* Post configure switches and hosts\n')
+    info('* Post configure switches and hosts\n')
 
     CLI(net)
     net.stop()
 
+
 if __name__ == '__main__':
-    setLogLevel( 'info' )
+    setLogLevel('info')
     myNetwork()
